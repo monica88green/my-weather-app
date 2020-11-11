672b15b5464276dae72323dba4fd635afffb0a02
monica88green <71725844+monica88green@users.noreply.github.com>
2020-11-11 14:40:52 -0700

    removed link from C°

--- src/index.js
+++ src/index.js
@@ -16,10 +16,10 @@ function formatDate(timestamp) {
     minutes = `0${minutes}`;
   }
 
-  h2.innerHTML = `${day}, ${date} ${hours}:${minutes}`;
+  h2.innerHTML = `${day}, ${date} ${hours}:${minutes}`
 }
-
 formatDate();
+
 
 function getCurrentWeather(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -34,6 +34,7 @@ function getCurrentWeather(response) {
     "#current-windspeed"
   ).innerHTML = `Windspeed: ${Math.round(response.data.wind.speed)}km/h`;}
 
+
 function searchCity(city) {
   let apiKey = `a6280162e920de8e2e14118aadbf3eac`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -41,11 +42,13 @@ function searchCity(city) {
   axios.get(apiUrl).then(getCurrentWeather);
 }
 
+
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#search-input").value;
   searchCity(city);
 }
+
 
 let form = document.querySelector("#city-search-form");
 form.addEventListener("submit", handleSubmit);
