5b98c6b745bd73b3d4dc770ff9dad28baef96f90
monica88green <71725844+monica88green@users.noreply.github.com>
2020-11-14 13:16:13 -0700

    Reformatted function formatDate

--- src/index.js
+++ src/index.js
@@ -1,40 +1,40 @@
-function formatDate() {
-  let currentDate = new Date();
-  let h2 = document.querySelector("#current-date-time");
-
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat", "Sun"];
-  let day = days[currentDate.getDay()];
-
-  let date = currentDate.getDate();
-  let hours = currentDate.getHours();
+function formatDate(timestamp) {
+  let date= new Date(timestamp);
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = currentDate.getHours();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = `0${minutes}`;}
-  
-  h2.innerHTML= `${day}, ${date} ${hours}:${minutes}`;
-}
-formatDate();
+    minutes = `0${minutes}`;
+  }
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
 
+  return `${day}, ${hours}:${minutes}`
+;}
  
 
 
 function getCurrentWeather(response) {
   celsiusTemp = response.data.main.temp;
 
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#city-temp").innerHTML = `${Math.round(
-    celsiusTemp
-  )}`;
-  document.querySelector("#weather-icon").setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-  document.querySelector(
-    "#current-humidity"
-  ).innerHTML = `Humidity: ${response.data.main.humidity}%`;
-  document.querySelector(
-    "#current-windspeed"
-  ).innerHTML = `Windspeed: ${Math.round(response.data.wind.speed)}km/h`;}
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#city-temp");
+  let iconElement = document.querySelector("#weather-icon");
+  let humidityElement = document.querySelector("#current-humidity");
+  let windElement = document.querySelector("#current-windspeed");
+  let dateElement = document.querySelector("#current-date-time");
+
+  cityElement.innerHTML = response.data.name;
+  temperatureElement.innerHTML = `${Math.round(celsiusTemp)}`;
+  iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  windElement.innerHTML = `Windspeed: ${Math.round(response.data.wind.speed)}km/h`;
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+
+}
 
 
 function getForecast(response) {
